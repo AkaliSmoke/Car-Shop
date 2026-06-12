ef75f54a7296a197568fb0f397e14d9628edf853
Kaio <kaiomascarenhas241@gmail.com>
2026-06-11 22:25:02 -0300

    Criando a tela de administração do Car Shop
    Implementando os botões que vão redirecionar para outras páginas criei o  botão para fechar o programa. Mudarei o Layout em breve.

--- Car-Shop/Form1.Designer.cs
+++ Car-Shop/Form1.Designer.cs
@@ -28,12 +28,112 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            lbTelaDeConsultas = new Label();
+            btnClientes = new Button();
+            btnVeiculos = new Button();
+            btnVendedor = new Button();
+            btnFechar = new Button();
+            lbCarShop = new Label();
+            label1 = new Label();
+            SuspendLayout();
+            // 
+            // lbTelaDeConsultas
+            // 
+            lbTelaDeConsultas.AutoSize = true;
+            lbTelaDeConsultas.Font = new Font("Yu Gothic Medium", 18F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lbTelaDeConsultas.ForeColor = Color.Red;
+            lbTelaDeConsultas.Location = new Point(68, 125);
+            lbTelaDeConsultas.Name = "lbTelaDeConsultas";
+            lbTelaDeConsultas.Size = new Size(224, 31);
+            lbTelaDeConsultas.TabIndex = 1;
+            lbTelaDeConsultas.Text = "Tela de Consultas";
+            // 
+            // btnClientes
+            // 
+            btnClientes.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnClientes.Location = new Point(12, 207);
+            btnClientes.Name = "btnClientes";
+            btnClientes.Size = new Size(128, 74);
+            btnClientes.TabIndex = 2;
+            btnClientes.Text = "Tela de Gerenciamento de Clientes";
+            btnClientes.UseVisualStyleBackColor = true;
+            // 
+            // btnVeiculos
+            // 
+            btnVeiculos.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnVeiculos.Location = new Point(209, 207);
+            btnVeiculos.Name = "btnVeiculos";
+            btnVeiculos.Size = new Size(128, 74);
+            btnVeiculos.TabIndex = 3;
+            btnVeiculos.Text = "Tela de Gerenciamento de Veiculos";
+            btnVeiculos.UseVisualStyleBackColor = true;
+            // 
+            // btnVendedor
+            // 
+            btnVendedor.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnVendedor.Location = new Point(12, 318);
+            btnVendedor.Name = "btnVendedor";
+            btnVendedor.Size = new Size(128, 78);
+            btnVendedor.TabIndex = 4;
+            btnVendedor.Text = "Tela de Gerenciamento de Vendedor";
+            btnVendedor.UseVisualStyleBackColor = true;
+            // 
+            // btnFechar
+            // 
+            btnFechar.Font = new Font("Segoe UI Semibold", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            btnFechar.Location = new Point(209, 318);
+            btnFechar.Name = "btnFechar";
+            btnFechar.Size = new Size(128, 78);
+            btnFechar.TabIndex = 5;
+            btnFechar.Text = "Fechar o Programa";
+            btnFechar.UseVisualStyleBackColor = true;
+            // 
+            // lbCarShop
+            // 
+            lbCarShop.AutoSize = true;
+            lbCarShop.Location = new Point(114, 55);
+            lbCarShop.Name = "lbCarShop";
+            lbCarShop.Size = new Size(0, 15);
+            lbCarShop.TabIndex = 6;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 14.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.ForeColor = SystemColors.Desktop;
+            label1.Location = new Point(45, 47);
+            label1.Name = "label1";
+            label1.Size = new Size(266, 25);
+            label1.TabIndex = 7;
+            label1.Text = "Tela Administrativa Car Shop";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(349, 460);
+            Controls.Add(label1);
+            Controls.Add(lbCarShop);
+            Controls.Add(btnFechar);
+            Controls.Add(btnVendedor);
+            Controls.Add(btnVeiculos);
+            Controls.Add(btnClientes);
+            Controls.Add(lbTelaDeConsultas);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Name = "Form1";
+            Text = "Tela Administrativa do Car Shop";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Label lbTelaDeConsultas;
+        private Button btnClientes;
+        private Button btnVeiculos;
+        private Button btnVendedor;
+        private Button btnFechar;
+        private Label lbCarShop;
+        private Label label1;
     }
 }
